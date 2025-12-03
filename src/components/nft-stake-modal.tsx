998db63a213491feeb4e3e98016b17d7b866165a
Hnng2011 <luphihung111@gmail.com>
2025-12-03 22:54:34 +0700

    update

--- src/components/nft-stake-modal.tsx
+++ src/components/nft-stake-modal.tsx
@@ -7,7 +7,7 @@ import {
 import { Stream } from "@/fake-data";
 import { cn } from "@/lib/utils";
 import { Card, CardContent } from "./ui/card";
-import { RarityColor } from "@/constants/rarity";
+import { MonthLabel, RarityColor } from "@/constants/rarity";
 import { Button } from "./ui/button";
 import { useEffect, useState } from "react";
 import { CircleCheck } from "lucide-react";
@@ -24,7 +24,9 @@ export function NFTStakeModal({
   onOpenChange,
   streams,
 }: NFTStakeModalProps) {
-  const [selected, setSelected] = useState<string[]>([]);
+  const [selected, setSelected] = useState<{ name: string; time: number }[]>(
+    []
+  );
 
   useEffect(() => {
     setSelected([]);
@@ -50,9 +52,9 @@ export function NFTStakeModal({
               <div
                 onClick={() =>
                   setSelected((prev) =>
-                    prev.includes(stream.name)
-                      ? prev.filter((name) => name !== stream.name)
-                      : [...prev, stream.name]
+                    prev.some((se) => se.name === stream.name)
+                      ? prev.filter((se) => se.name !== stream.name)
+                      : [...prev, { name: stream.name, time: 1 }]
                   )
                 }
                 key={stream.name}
@@ -61,14 +63,16 @@ export function NFTStakeModal({
                   <div
                     className={cn(
                       "opacity-0 bg-black absolute inset-0 z-10 duration-100",
-                      selected.includes(stream.name) && "opacity-50"
+                      selected.some((sel) => sel.name === stream.name) &&
+                        "opacity-50"
                     )}
                   />
 
                   <div
                     className={cn(
                       "opacity-0 absolute right-4 top-4 z-20 duration-100",
-                      selected.includes(stream.name) && "delay-300 opacity-100"
+                      selected.some((sel) => sel.name === stream.name) &&
+                        "delay-300 opacity-100"
                     )}
                   >
                     <CircleCheck className="w-6 h-6 text-purple-700" />
@@ -84,40 +88,74 @@ export function NFTStakeModal({
 
                   {/* Overlay Content */}
                   <CardContent className="relative z-10 flex flex-col items-center justify-between h-90 text-center">
-                    <div
-                      className={cn(
-                        "flex items-end gap-2 absolute top-0 delay-100",
-                        selected.includes(stream.name) &&
-                          "-translate-y-20 duration-300 delay-0"
-                      )}
-                    >
-                      <div
-                        className={cn(
-                          "py-1 px-3 rounded-lg border text-xs",
-                          RarityColor[stream.rarity as keyof typeof RarityColor]
-                        )}
-                      >
-                        {stream.rarity.toUpperCase()}
-                      </div>
-                      <div
-                        className={cn(
-                          "py-1 px-3 rounded-lg border text-xs",
-                          RarityColor[stream.rarity as keyof typeof RarityColor]
-                        )}
-                      >
-                        {stream.power}%
-                      </div>
-                    </div>
+                    {!selected.some((sel) => sel.name === stream.name) ? (
+                      <>
+                        <div
+                          className={cn(
+                            "flex items-end gap-2 absolute top-0 delay-100",
+                            selected.some((sel) => sel.name === stream.name) &&
+                              "-translate-y-20 duration-300 delay-0"
+                          )}
+                        >
+                          <div
+                            className={cn(
+                              "py-1 px-3 rounded-lg border text-xs",
+                              RarityColor[
+                                stream.rarity as keyof typeof RarityColor
+                              ]
+                            )}
+                          >
+                            {stream.rarity.toUpperCase()}
+                          </div>
+                          <div
+                            className={cn(
+                              "py-1 px-3 rounded-lg border text-xs",
+                              RarityColor[
+                                stream.rarity as keyof typeof RarityColor
+                              ]
+                            )}
+                          >
+                            {stream.power}%
+                          </div>
+                        </div>
 
-                    <h3
-                      className={cn(
-                        "text-xl font-bold text-white tracking-wide absolute bottom-0 delay-100",
-                        selected.includes(stream.name) &&
-                          "translate-y-20 duration-300 delay-0"
-                      )}
-                    >
-                      {stream.name}
-                    </h3>
+                        <h3
+                          className={cn(
+                            "text-xl font-bold text-white tracking-wide absolute bottom-0 delay-100",
+                            selected.some((sel) => sel.name === stream.name) &&
+                              "translate-y-20 duration-300 delay-0"
+                          )}
+                        >
+                          {stream.name}
+                        </h3>
+                      </>
+                    ) : (
+                      <div className="text-white flex flex-col h-full w-full items-center justify-center gap-5">
+                        {Object.entries(MonthLabel).map(([key, value]) => (
+                          <Button
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              setSelected((prev) => [
+                                ...prev.filter(
+                                  (sel) => sel.name !== stream.name
+                                ),
+                                { name: stream.name, time: Number(key) },
+                              ]);
+                            }}
+                            className={cn(
+                              "w-full bg-zinc-600 text-black",
+                              selected.find((sel) => sel.name === stream.name)
+                                ?.time === Number(key)
+                                ? "bg-zinc-300"
+                                : ""
+                            )}
+                            key={key}
+                          >
+                            {value}
+                          </Button>
+                        ))}
+                      </div>
+                    )}
                   </CardContent>
                 </Card>
               </div>
